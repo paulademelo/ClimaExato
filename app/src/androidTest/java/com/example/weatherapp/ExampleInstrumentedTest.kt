b2f20da309705c4edbdc3ae35aece03855e0f532
paulademelo <paulinhansilva@gmail.com>
2024-07-18 18:56:53 -0300

    [refactor] code

--- app/src/androidTest/java/com/example/weatherapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/weatherapp/ExampleInstrumentedTest.kt
@@ -1,11 +1,5 @@
 package com.example.weatherapp
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithContentDescription
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -13,8 +7,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
-import org.junit.Before
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,20 +15,10 @@ import org.junit.Rule
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
     @Test
     fun useAppContext() {
+        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.weatherapp", appContext.packageName)
-    }
-
-    @Test
-    fun testWeatherInfoDisplaysCorrectData() {
-        composeTestRule.onNodeWithText("Clima Exato").assertIsDisplayed()
-        Thread.sleep(5000)
-        composeTestRule.onNodeWithText("São Paulo").assertIsDisplayed()
-        composeTestRule.onNodeWithContentDescription("Ícone relacionado ao tempo").assertIsDisplayed()
     }
 }
