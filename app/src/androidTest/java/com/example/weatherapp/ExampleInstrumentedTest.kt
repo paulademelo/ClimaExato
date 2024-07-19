39995098c664b682a2ef8bf4d142300bd9c8cdaa
paulademelo <paulinhansilva@gmail.com>
2024-07-18 21:19:38 -0300

    [add] instrumented test for splash

--- app/src/androidTest/java/com/example/weatherapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/weatherapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.weatherapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.activity.compose.setContent
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.weatherapp.presentation.MainScreen
+import com.example.weatherapp.presentation.SplashScreen
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,15 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.weatherapp", appContext.packageName)
+    fun testSplashScreenDisplaysText() {
+        composeTestRule.setContent {
+            SplashScreen()
+        }
+        composeTestRule.onNodeWithText("Clima Exato").assertIsDisplayed()
     }
 }
